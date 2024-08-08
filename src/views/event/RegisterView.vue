e7161db34237314a4fe51a4c2fab7395d15cabff
Haoxuan Yu <haoxuan_yu@cmu.ac.th>
2024-08-09 00:06:10 +0800

    3.2

---
+++ src/views/event/RegisterView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { toRef} from 'vue'
+import { type Event } from '@/types'
+
+const props = defineProps<{
+    event: Event
+}>()
+// eslint-lisable-next-line @typescript-eslint/no-unused-vars
+const { event } = toRef(props)
+</script>
+
+<template>
+    <p>Register event here</p>
+</template>
