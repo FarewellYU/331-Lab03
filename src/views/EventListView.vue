7f75099803280187e4b593bf402db9bf57802c65
Haoxuan Yu <haoxuan_yu@cmu.ac.th>
2024-08-08 23:41:12 +0800

    2.7 change page function

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import EventCard from '@/components/EventCard.vue'
-import Event from '@/types/Event'
+import { type Event } from '@/types'
 import EventDetails from '@/components/EventDetails.vue'
-import { ref, onMounted,computed } from 'vue'
+import { ref, onMounted,computed, watchEffect } from 'vue'
 import EventService from '@/services/EventService'
+import { error } from 'console'
 
-const events = ref<Event[]>(null)
+const events = ref<Event[] | null>(null)
 const props = defineProps({
   page: {
     type : Number,
@@ -14,14 +15,16 @@ const props = defineProps({
 })
 const page = computed(() => props.page)
 onMounted(() =>{
-  EventService.getEvents(2, page.value)
-    .then((response) =>{
-       //console.log(response.data)
+  watchEffect(() => {
+    events.value = null
+    EventService.getEvents(2, page.value)
+    .then((response) => {
       events.value = response.data
     })
-    .catch((error) =>{
+    .catch((error) => {
       console.error('There was an error!', error)
     })
+  })
   })
 </script>
 
