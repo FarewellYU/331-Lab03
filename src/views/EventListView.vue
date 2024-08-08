75b7d224dd7f95623590d7d301b0dad7c7a8fd14
Haoxuan Yu <haoxuan_yu@cmu.ac.th>
2024-08-08 23:32:17 +0800

    2.5 previous and next page

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -32,6 +32,17 @@ onMounted(() =>{
     <EventCard v-for="event in events" :key="event.id" :event="event" />
     <EventDetails v-for="event in events" :key="event.id" :event="event" />
   </div>
+  <RouterLink
+  :to="{ name: 'event-list-view', query: {page : page - 1}}"
+  rel = "prev"
+  v-if = "page !=1"
+  >Prev page</RouterLink
+  >
+
+  <RouterLink :to ="{ name: 'event-list-view', query: {page: page +1}}"
+  rel="next"
+  >Next Page</RouterLink
+  >
 </template>
 
 <style scoped>
